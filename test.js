502b19bee960434ce2382f9127dc4fe9bb2b1df1
NOOR OMARI <nooromari847@gmail.com>
2021-03-07 19:24:30 +0200

    test files

---
+++ test.js
@@ -0,0 +1,72 @@
+'use strict';
+function getRandomArbitrary(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+let tableDiv=document.getElementById('tableDiv');
+let tableEl=document.createElement('table');
+tableDiv.appendChild(tableEl);
+
+let objectArr=[];
+
+function Animal(name,maxNum,minNum){
+  this.name=name;
+  this.maxNum=maxNum;
+  this.minNum=minNum;
+  this.age=this.ageRandom();
+  objectArr.push(this);
+}
+Animal.prototype.ageRandom=function(){
+  return this.age=getRandomArbitrary(this.minNum,this.maxNum);
+};
+
+new Animal('dog',10,5);
+new Animal('cat',7,3);
+new Animal('rabbit',14,8);
+console.log(objectArr);
+
+
+document.getElementById('animalF').addEventListener('submit',animalForm);
+
+function animalForm(event) {
+  event.preventDefault();
+  //   console.log(min.value);
+  new Animal(Aname.value,max.value,min.value);
+  //   for(let i = 0 ; i<tableEl.rows.length ;){
+  saveObj();
+  while(tableEl.rows.length){
+    console.log(tableEl.rows.length);
+    tableEl.deleteRow(0);
+  }
+  tableRender();
+}
+
+function tableRender(){
+  let trEl=document.createElement('tr');
+  tableEl.appendChild(trEl);
+  let thEl=document.createElement('th');
+  trEl.appendChild(thEl);
+  thEl.textContent='Name';
+
+  let thEl1=document.createElement('th');
+  trEl.appendChild(thEl1);
+  thEl1.textContent='Age';
+
+  for (let i = 0; i < objectArr.length; i++) {
+    let trEl=document.createElement('tr');
+    tableEl.appendChild(trEl);
+    let tdEl=document.createElement('td');
+    trEl.appendChild(tdEl);
+    tdEl.textContent=objectArr[i].name;
+
+    let tdEl1=document.createElement('td');
+    trEl.appendChild(tdEl1);
+    tdEl1.textContent=objectArr[i].age;
+  }
+}
+tableRender();
+
+function saveObj() {
+  let save = JSON.stringify(objectArr);
+  let allAnimals =localStorage.setItem('allAnimals',save);
+}
